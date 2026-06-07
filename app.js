5657d82a67a97eabe6e50e2d7c4566b2171ee3b7
Saim_Mujahid <saimwaiz277@gmail.com>
2026-06-07 16:10:31 +0500

    Weather App

---
+++ app.js
@@ -0,0 +1,32 @@
+// Shows Date
+
+const today = new Date();
+document.getElementById("date").textContent = today.toDateString();
+
+// Collects Input! 
+let CityDiv = document.querySelector("#city-result");
+let TempDiv = document.querySelector("#temp-result");
+let HumidityDiv = document.querySelector("#humidity-result");
+let WindDiv = document.querySelector("#wind-result");
+let RegionDiv = document.querySelector("#region-result");
+let StatusDiv = document.querySelector("#status-result");
+let ImgTag = document.querySelector("#img-icon")
+
+
+// Function 
+async function WeatherApp (event){
+    let city = document.querySelector("#CityName").value;
+    event.preventDefault();
+    let response = await axios(`https://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=${city}`,);
+    
+    
+  // Collects Output 
+     CityDiv.innerHTML = response.data.location.name;
+     TempDiv.innerHTML = response.data.current.temp_c + "°C";
+     HumidityDiv.innerHTML = response.data.current.humidity + "%";
+     WindDiv.innerHTML = response.data.current.wind_kph + " Km/h";
+     RegionDiv.innerHTML = response.data.location.region + ", " + response.data.location.country;
+     StatusDiv.innerHTML = response.data.current.condition.text;
+     let icon = response.data.current.condition.icon
+     ImgTag.src = icon
+}
